c9bca6491c8dc4dc2bab9625c85e7d502500db66
xiung <steven19790906@gmail.com>
2025-06-02 23:01:55 +0800

    feat: 初始化

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, computed, provide } from 'vue'
+import Home from '@/components/Home.vue'
+import Intro from '@/components/Intro.vue'
+</script>
+
+<template>
+  <div id="bg" class="fixed inset-0 -z-10 bg-cover bg-center"></div>
+  <div id="bg-wrapper" class="fixed inset-0 -z-10 bg-black opacity-50"></div>
+
+  <div class="w-full max-w-4xl flex flex-col items-center justify-center mx-auto relative min-h-screen z-10">
+    <Home />
+    <Intro />
+  </div>
+
+</template>
+
+<style>
+#bg {
+  background-image: url('@/assets/img/bg.jpg');
+  /* 用 @ 指定 Vite 的靜態路徑 */
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="title" class="sticky top-0 mt-[40vh]">
+        <!-- 初始置中顯示（只影響初始位置） -->
+        <div class="flex items-center justify-center">
+            <div class="text-center text-white py-6 px-4">
+                <h1 class="text-4xl">〈留下來的不止影像〉</h1>
+                <p class="text-2xl opacity-70">有「性」可談</p>
+            </div>
+        </div>
+    </div>
+    <div class="text-white text-center flex flex-col items-center justify-center z-20 mt-15 mb-[27vh]">
+        <svg class="animate__animated animate__shakeY animate__infinite animate__slower w-20 h-20" aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M12 19V5m0 14-4-4m4 4 4-4" />
+        </svg>
+    </div>
+</template>
+
+<style>
+#title {
+    font-family: 'Noto Serif TC', sans-serif;
+    font-weight: 600;
+}
+</style>
